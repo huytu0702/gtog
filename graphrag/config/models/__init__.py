f24e6db7a5e66976f457c268b5aaaf21a4c98f48
huytu0702 <tufy2k4@gmail.com>
2026-01-29 19:12:14 +0700

    feat(config): export EvalConfig from models package

---
+++ graphrag/config/models/__init__.py
@@ -0,0 +1,33 @@
+# Copyright (c) 2024 Microsoft Corporation.
+# Licensed under the MIT License
+
+"""Configuration models package exports."""
+
+from graphrag.config.models.basic_search_config import BasicSearchConfig
+from graphrag.config.models.cache_config import CacheConfig
+from graphrag.config.models.chunking_config import ChunkingConfig
+from graphrag.config.models.cluster_graph_config import ClusterGraphConfig
+from graphrag.config.models.community_reports_config import CommunityReportsConfig
+from graphrag.config.models.drift_search_config import DriftSearchConfig
+from graphrag.config.models.embed_graph_config import EmbedGraphConfig
+from graphrag.config.models.eval_config import EvalConfig
+from graphrag.config.models.extract_claims_config import ExtractClaimsConfig
+from graphrag.config.models.extract_graph_config import ExtractGraphConfig
+from graphrag.config.models.extract_graph_nlp_config import ExtractGraphNlpConfig
+from graphrag.config.models.global_search_config import GlobalSearchConfig
+from graphrag.config.models.input_config import InputConfig
+from graphrag.config.models.language_model_config import LanguageModelConfig
+from graphrag.config.models.local_search_config import LocalSearchConfig
+from graphrag.config.models.prune_graph_config import PruneGraphConfig
+from graphrag.config.models.reporting_config import ReportingConfig
+from graphrag.config.models.snapshots_config import SnapshotsConfig
+from graphrag.config.models.storage_config import StorageConfig
+from graphrag.config.models.summarize_descriptions_config import (
+    SummarizeDescriptionsConfig,
+)
+from graphrag.config.models.text_embedding_config import TextEmbeddingConfig
+from graphrag.config.models.tog_search_config import ToGSearchConfig
+from graphrag.config.models.vector_store_config import VectorStoreConfig
+from graphrag.config.models.vector_store_schema_config import (
+    VectorStoreSchemaConfig,
+)
